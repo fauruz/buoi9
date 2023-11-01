3d74ad3d58fd897ef907cb56bb34e2b257645845
fauruz <le.phong1456@gmail.com>
2023-11-01 16:19:42 +0700

    bai1.3

--- b1/Form1.cs
+++ b1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using b1.Model;
+using b1.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,13 +21,40 @@ namespace b1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Department();
+        }
+        private void Department()
+        {
             ConnectDepartment obj = new ConnectDepartment();
             List<Department> ldp = obj.getData();
-            foreach(var item in ldp)
+            foreach (var item in ldp)
             {
                 cbbDepartment.Items.Add(item.DepartmentName);
-                cbbDepartment.SelectedIndex = 0;
-            }    
+            }
         }
+        private void txtAuto_TextChanged(object sender, EventArgs e)
+        {
+            ConnectStudent obj = new ConnectStudent();
+            List<Student> lsd = obj.getData();
+            bool b = false;
+            foreach (Student sd in lsd)
+            {
+                if (Convert.ToInt32(txtAuto.Text) == sd.Id)
+                {
+                    txtId.Text = txtAuto.Text;
+                    txtName.Text = sd.Name;
+                    cbbGender.SelectedIndex = sd.Gender;
+                    txtYear.Text = sd.Year.ToString();
+                    cbbDepartment.SelectedIndex = sd.DpId-1;
+                    b = true;
+                }
+            }
+            if (!b)
+            {
+                MessageBox.Show("Không tìm thấy mã sinh viên!");
+            }
+        }
+
+
     }
 }
---
+++ b1/Models/ConnectStudent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b1.Models
+{
+    internal class ConnectStudent
+    {
+        public List<Student> getData()
+        {
+            List<Student> lsd = new List<Student>();
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                da.SelectCommand = new SqlCommand();
+                da.SelectCommand.CommandText = "select * from Student";
+                da.SelectCommand.Connection = con;
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    Student sd = new Student();
+                    sd.Id = Convert.ToInt32(row["Id"].ToString());
+                    sd.Name = row["Name"].ToString();
+                    sd.Gender = Convert.ToInt32(row["Gender"].ToString());
+                    sd.Year = Convert.ToInt32(row["Year"].ToString());
+                    sd.DpId = Convert.ToInt32(row["DpId"].ToString());
+                    lsd.Add(sd);
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return lsd;
+        }
+    }
+}
