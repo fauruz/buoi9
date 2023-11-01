3ab29b523dd7d97e0f15057fd9c9ea69848440ac
fauruz <le.phong1456@gmail.com>
2023-11-01 13:40:53 +0700

    bai1.1

---
+++ b1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using b1.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConnectDepartment obj = new ConnectDepartment();
+            List<Department> ldp = obj.getData();
+            foreach(var item in ldp)
+            {
+                cbbDepartment.Items.Add(item);
+            }    
+        }
+    }
+}
---
+++ b1/Models/ConnectDepartment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using b1.Model;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace b1.Model
+{
+    internal class ConnectDepartment
+    {
+        public List<Department> getData()
+        {
+            List<Department> ldp = new List<Department>();
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            string sql = "Select * from Department";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Department dp = new Department();
+                dp.DepartmentId = Convert.ToInt32(rdr.GetString(0));
+                dp.DepartmentName = rdr.GetString(1);
+                ldp.Add(dp);
+            }
+            return ldp;
+        }
+    }
+}
